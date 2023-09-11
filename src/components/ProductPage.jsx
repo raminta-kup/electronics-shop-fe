4cb0f2dee93b9c88dc6e799386e9236dd3d668de
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2023-09-11 12:12:53 +0300

    Fetched data, implemented, finished Product Page

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,15 +1,36 @@
 import { styled } from "styled-components";
-import { products } from "../data/productsCatalog"
 import { ProductItem } from "./ProductItem";
 import { ProductCategoryHeader } from "./ProductCategoryHeader";
 import { ProductCategoryList } from "./ProductCategoryList";
 import { devices } from "../ScreenSizes/screenSizes";
 import { AudiophileExperience } from "./AudiophileExperience";
 import { Wrapper } from "./Wrapper";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 
 export const ProductPage = ({ productCategory }) => {
-    const filteredProductsByCategory = products.filter(product => product.category === productCategory);
+    const [products, setProducts] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const filteredProductsByCategory = products?.filter(product => product.category === productCategory);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/products`)
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data);
+                setIsLoading(false);
+            })
+    }, []);
+
+    //using "?" (in product?.name) for safety, because if "products" was null or undefined it would throw an error. In this case, it would just show undefined.
+
+    if (isLoading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
 
     return (
         <>
@@ -17,9 +38,12 @@ export const ProductPage = ({ productCategory }) => {
             <Wrapper>
                 <StyledHeadphonesSectionContainer>
                     <StyledProductsContainer>
-                        {filteredProductsByCategory.map(productCategory => {
+                        {filteredProductsByCategory?.map(product => {
                             return (
-                                <ProductItem key={productCategory.id} product={productCategory} />
+                                <ProductItem
+                                    key={product?.slug}
+                                    product={product}
+                                />
                             )
                         })}
                     </StyledProductsContainer>
