f594d4c0cbd0d21d6417aa45726743c6a28b2250
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2023-09-14 11:25:22 +0300

    fixed amount of products in cart on cart img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,15 @@ import shoppingCart from "../assets/icons/Combined Shape.png"
 import { Hamburger } from "./buttons/Hamburger";
 import { devices } from "../ScreenSizes/screenSizes";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Cart } from "./Cart";
 import { Menu } from "./buttons/Menu";
+import CartContext from "../CartContext";
 
 export const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
+    const { cart } = useContext(CartContext);
 
     const handleOpenCart = () => {
         setIsOpen(true);
@@ -42,6 +44,9 @@ export const Header = () => {
                     onClick={handleOpenCart}
                 >
                     <img src={shoppingCart} />
+                    <ItemsInCartSpan>
+                        {cart.length === 0 ? "0" : cart.reduce((sum, item) => sum + item.quantity, 0)}
+                    </ItemsInCartSpan>
                 </ShoppingCartBtn>
                 <Cart
                     open={isOpen}
@@ -67,6 +72,22 @@ const HeaderWrapper = styled.div`
 
 `
 
+const ItemsInCartSpan = styled.span`
+    background-color: #D87D4A;
+    color: #FFF;
+    font-weight: 600;
+    position: absolute;
+    border-radius: 50%;
+    height: 10px;
+    width: 10px;
+    padding: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: -6px;
+    right: -6px;
+`
+
 const StyledHeaderContainer = styled.header`
     display: flex;
     border-bottom: 1px solid #303030;
@@ -84,6 +105,9 @@ const ShoppingCartBtn = styled.button`
     background-color: transparent;
     border: transparent;
     cursor: pointer;
+    position: relative;
+    display: flex;
+    align-items: center;
 `
 
 const StyledHamburgerBtn = styled.button`
